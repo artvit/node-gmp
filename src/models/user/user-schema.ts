91278218655656f564004cc956f2742b5276e4cc
Artem Vitkovsky <artvitk@gmail.com>
2020-08-19 10:15:10 +0300

    Update password validation

--- src/models/user/user-schema.ts
+++ src/models/user/user-schema.ts
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export const userSchema = Joi.object({
   login: Joi.string().alphanum().min(3).max(50).required(),
-  password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+  password: Joi.string().pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=(.*[0-9].*){2,}).{8,64}$/).required(),
   age: Joi.number().integer().min(4).max(130).required()
 });
